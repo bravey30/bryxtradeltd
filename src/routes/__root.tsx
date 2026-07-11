90118dc583e1a3e5c5a197cb52dc1da40661bd5a
mbalike <dmbalike@outlook.com>
2026-07-11 15:27:01 +0300

    added whatsapp button

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,6 +11,7 @@ import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
+import { FloatingWhatsAppButton } from "../components/floating-whatsapp-button";
 
 function NotFoundComponent() {
   return (
@@ -108,6 +109,7 @@ function RootComponent() {
   return (
     <QueryClientProvider client={queryClient}>
       <Outlet />
+      <FloatingWhatsAppButton />
     </QueryClientProvider>
   );
 }
